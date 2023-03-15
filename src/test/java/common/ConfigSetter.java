0886af62195a2afad1fd3caef04b092d802695e2
Karalina199 <carolina.chernenok@gmail.com>
2023-03-15 16:18:34 +0300

    Init commit

--- src/test/java/common/ConfigSetter.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public class ConfigSetter {
-
-     public static final String PLATFORM_AND_BROWSER = "win_chrome";
-
-     public static final Boolean HOLD_BROWSER_OPEN = true;
-}
